3742a1d5b0811984d324854eeaeb98dc43c9ddfa
marco <marco@MAREK-LAPTOP>
2022-03-10 09:51:05 +0100

    Přidejte soubory projektu.

---
+++ vesi mensi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Zadejte 1. číslo");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Zadejte číslo 2");
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+if (cislo1 < cislo2)
+{
+    Console.WriteLine($"{cislo2} je vetsi nez {cislo1}");
+}
+else
+{
+    Console.WriteLine($"{cislo1} je vetsi nez {cislo2}");
+}
